c999ab0603247fe1b77f4f17f077cafee2f95322
araltasher <araltasher@gmail.com>
2018-03-09 15:21:33 -0500

    completed IIFE, first class functions and closures

--- 5-Advanced-JS/script.js
+++ 5-Advanced-JS/script.js
@@ -90,7 +90,7 @@ console.log(obj.city);
 */
 
 //  Passing Functions as arguments
-
+/*
 var years = [1990, 1965, 1937, 2005, 1998];
 
 function arrayCalc(arr, fn) {
@@ -124,3 +124,66 @@ var rates = arrayCalc(ages, maxHeartRate);
 console.log(ages);
 console.log(fullAges);
 console.log(rates);
+*/
+
+// Functions returning another function
+/*
+function interviewQuestion(job) {
+    if (job === 'designer') {
+        return function (name) {
+            console.log(name + ', can you please explain what UX design is?');
+        }
+    } else if (job === 'teacher') {
+        return function (name) {
+            console.log('What subject do you teach, ' + name + '?');
+        }
+    } else {
+        return function (name) {
+            console.log('Hello ' + name + ', what do you do?');
+        }
+    }
+}
+
+var teacherQuestion = interviewQuestion('teacher');
+var designerQuestion = interviewQuestion('designer');
+teacherQuestion('John');
+designerQuestion('John');
+*/
+
+//  IIFE
+/*(function () {
+    var score = Math.random() * 10;
+    console.log(score >= 5);
+})();
+*/
+
+//  Closures
+function retirement(retirementAge) {
+    let a = ' years left until retirement.';
+    return function (yearOfBirth) {
+        var age = 2018 - yearOfBirth;
+        console.log((retirementAge - age) + a);
+    }
+}
+
+var retirementUS = retirement(66);
+retirementUS(1992);
+var retirementGermany = retirement(65);
+retirementGermany(1992);
+var retirementIceland = retirement(67);
+retirementIceland(1992);
+
+
+function interviewQuestion(job) {
+    return function (name) {
+        if (job === 'designer') {
+            console.log(name + ', can you please explain what UX design is?');
+        } else if (job === 'teacher') {
+            console.log('What subject do you teach, ' + name + '?');
+        } else {
+            console.log('Hello ' + name + ', what do you do?');
+        }
+    }
+}
+
+interviewQuestion('teacher')('Jane');
