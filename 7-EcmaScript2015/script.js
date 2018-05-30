ef3b690abbe76ba7bdfd7ec1e20c8d38eb5aa690
Aral Tasher <araltasher@Arals-iMac-5K.local>
2018-05-30 11:03:50 -0400

    arrow functions completed

--- 7-EcmaScript2015/script.js
+++ 7-EcmaScript2015/script.js
@@ -35,7 +35,7 @@ function driversLicense6(passedTest) {
     console.log(firstName + ' born in ' + yearOfBirth + ', is now officially allowed to drive a car.');
 };
 driversLicense6(true);
-*/
+
 
 /////////////////////////////////////
 // LECTURE: BLOCKS and IIFEs
@@ -54,3 +54,114 @@ console.log(c);
 (function () {
     var c = 3;
 })();
+
+
+/////////////////////////////////////
+// LECTURE: STRINGS
+
+let firstName = 'John';
+let lastName = 'Smith';
+const yearOfBirth = 1990;
+
+function calcAge(year) {
+    return 2018 - year;
+}
+
+//ES5
+console.log('This is ' + firstName + ' ' + lastName + '. He was born in ' + yearOfBirth + '. Today, he is ' + calcAge(yearOfBirth) + ' years old.');
+
+//ES6
+console.log(`This is ${firstName} ${lastName}. He was born in ${yearOfBirth}. Today, he is ${calcAge(yearOfBirth)} years old.`);
+
+const n = `${firstName} ${lastName}`;
+
+console.log(n.startsWith('J'));
+console.log(n.endsWith('th'));
+console.log(n.includes('ar'));
+console.log(`${firstName} `.repeat(5));
+
+
+/////////////////////////////////////
+// LECTURE: ARROW FUNCTIONS -- Part 1
+
+const years = [1990, 1965, 1982, 1937];
+
+//ES5
+var ages5 = years.map(function(el) {
+  return 2018 - el;
+});
+console.log(ages5);
+
+//ES6
+let ages6 = years.map(el => 2018 - el);
+console.table(ages6);
+
+ages6 = years.map((el, index) => `Age element ${index + 1}: ${2018 - el}.`);
+console.log(ages6);
+
+ages6 = years.map((el, index) => {
+  const now = new Date().getFullYear();
+  const age = now - el;
+
+  return `Age element ${index + 1}: ${age}.`;
+});
+console.log(ages6);
+*/
+
+/////////////////////////////////////
+// LECTURE: ARROW FUNCTIONS -- Part 2
+
+//ES5
+var box5 = {
+    color: "green",
+    position: 1,
+    clickMe: function () {
+        var self = this;
+        document.querySelector(".green").addEventListener("click", function () {
+            var str =
+                "This is box number " + self.position + ", and it is " + self.color;
+            alert(str);
+        });
+    }
+};
+//box5.clickMe();
+
+//ES6
+var box6 = {
+    color: "green",
+    position: 1,
+    clickMe: function () {
+        document.querySelector(".green").addEventListener("click", () => {
+            alert(`This is box number ${this.position}, and it is ${this.color}`);
+        });
+    }
+};
+box6.clickMe();
+
+
+function Person(name) {
+    this.name = name;
+}
+
+var friends = ['Bob', 'Jane', 'Mark'];
+
+//ES5
+Person.prototype.myFriends5 = function (friends) {
+    var arr = friends.map(function (el) {
+        return this.name + ' is friends with ' + el;
+    }.bind(this));
+
+    console.log(arr);
+}
+
+new Person('John').myFriends5(friends);
+
+//ES6
+Person.prototype.myFriends6 = function (friends) {
+    var arr = friends.map(el =>
+        `${this.name} is friends with ${el}`);
+
+    console.log(arr);
+}
+
+new Person('Mike').myFriends6(friends);
